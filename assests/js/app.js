941c4609e4fb496992bfc5864f045f51ffee6361
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-02-28 10:58:13 +0530

    ui issues done

---
+++ assests/js/app.js
@@ -0,0 +1,44 @@
+
+$(".news_slider").slick({
+  infinite: true,
+  slidesToShow: 3.2,
+  centerMode: true,
+  centerPadding: "75px",
+  slidesToScroll: 1,
+  prevArrow: $(".slick-prev"),
+  nextArrow: $(".slick-next"),
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2.2,
+        centerPadding: "82px",
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2.1,
+        centerPadding: "40px",
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1.1,
+        centerPadding: "55px",
+        autoplay: true,
+        autoplaySpeed: 2000,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1.1,
+        centerPadding: "40px",
+        autoplay: true,
+        autoplaySpeed: 2000,
+      },
+    },
+  ],
+});
